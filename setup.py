14fd8a472b48d0b917f18fbcd85c5f2f90ddc297
Marcus Lewis <git@probablymarcus.com>
2023-10-01 11:36:58 -0700

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(name="vexpr",
-      version="0.0.1",
-      description="Write your code as tree-like expressions, then transform it",
+      version="0.1.0",
+      description="Represent code expressions as data structures, then transform them",
       author="Marcus Lewis",
-      url="https://outerloop.xyz/",
+      url="https://vexpr.org/",
       packages=find_packages(),
 )
